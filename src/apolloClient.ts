f78abc452499a91d44afa8ed505a7dfd7b8f6e1f
Enzo Do rosario <dorosario.enzo@gmail.com>
2021-09-07 14:36:51 +0200

    fix endpoints

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export default new ApolloClient({
-  uri: process.env.GRAPHQL_API_URI ?? "http://api.atypikhou.se/graphql",
+  uri: process.env.API_URL ?? "http://localhost:4000/graphql",
   credentials: "include",
   cache: new InMemoryCache(),
 });
