d7b6eb8376999f29b506590f635d96640b7fc3c2
Enzo Do rosario <dorosario.enzo@gmail.com>
2021-09-07 15:09:02 +0200

    fix endpoint

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export default new ApolloClient({
-  uri: process.env.API_URL ?? "http://localhost:4000/graphql",
+  uri: process.env.REACT_APP_API_URL ?? "http://localhost:4000/graphql",
   credentials: "include",
   cache: new InMemoryCache(),
 });
